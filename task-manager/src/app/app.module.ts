df74c43598e1d5ae18b83369160202c799576111
Jernej Korosec <jernejk.alfa@gmail.com>
2022-02-05 00:03:31 +0100

    Routes

--- task-manager/src/app/app.module.ts
+++ task-manager/src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './views/login/login.component';
+import { TasksComponent } from './views/tasks/tasks.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    TasksComponent    // components that are NOT in its own module
   ],
   imports: [
-    BrowserModule
+    BrowserModule,    // componenets in own module, own namespace!
+    AppRoutingModule  // componenets in own module, own namespace!
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [],    // Providers share code between conmponents
+  bootstrap: [AppComponent]  // main component entry point
 })
 export class AppModule { }

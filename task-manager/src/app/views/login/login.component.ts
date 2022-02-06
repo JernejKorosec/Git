026a4dfa7c03fbf21acbc9c2ddd1ec59874e44cf
Jernej Korosec <jernejk.alfa@gmail.com>
2022-02-06 23:43:02 +0100

    bugfix 2

--- task-manager/src/app/views/login/login.component.ts
+++ task-manager/src/app/views/login/login.component.ts
@@ -1,75 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-
 import { UserService } from 'src/app/services/mock/user.service';
 import { Router } from '@angular/router';
 import { UserModel } from 'src/app/model/user-model';
-
-//for authenticate
-import {UserVO} from "../../vo/user-vo";
-import {HttpErrorResponse} from "@angular/common/http";
+import { UserVO } from "../../vo/user-vo";
+import { HttpErrorResponse } from "@angular/common/http";
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
+
 export class LoginComponent implements OnInit {
 
   username = '';
   password = '';
+
   loginError = '';
   usernameError = '';
   passwordError = '';
 
-
-
-  onReset(): void {
-    this.username = '';
-    this.password = '';
-
-    console.log('onReset');
-  }
-  onLogin(): void {
-    let errorFound: boolean = false;
-
-    if (this.username === '') {
-      this.usernameError = 'You Must Enter a Username';
-      errorFound = true;
-    } else {
-      this.usernameError = '';
-    }
-    
-    if (this.password === '') {
-      this.passwordError = 'You Must Enter a Password';
-      errorFound = true;
-    } else {
-      this.passwordError = '';
-    }
-    
-    if (errorFound === true) {
-      return;
-    }
-    this.loginError = '';
-
-    this.userService.authenticate( this.username, this.password ).subscribe({
-      next: (result: UserVO) => {
-        // result code
-        console.log("login.component.ts this.userService.authenticate.next ");
-        this.userModel.user = result;
-        console.log("this.userModel.user = result;");
-        this.router.navigate(['/tasks']);
-        console.log("this.router.navigate(['/tasks']);;");
-
-      },
-      error: (error: HttpErrorResponse) => {
-        // error code
-      }
-    }
-  );
-  
-    console.log('onLogin');
-  }
   constructor(
     private userService: UserService, // kopoonente katere v kontruktorju deifiniramo 
     private router: Router,
@@ -78,4 +29,47 @@ export class LoginComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  onReset(): void {
+    this.username = '';
+    this.password = '';
+
+    console.log('onReset');
+  }
+
+  onLogin(): void {
+    let errorFound: boolean = false;
+    this.loginError = '';
+
+    if (this.username === '') {
+      this.usernameError = 'You Must Enter a Username';
+      errorFound = true;
+    } else {
+      this.usernameError = '';
+    }
+
+    if (this.password === '') {
+      this.passwordError = 'You Must Enter a Password';
+      errorFound = true;
+    } else {
+      this.passwordError = '';
+    }
+
+    if (errorFound === true) {
+      return;
+    }
+    
+
+    this.userService.authenticate(this.username, this.password).
+    subscribe
+    ({
+      next: (result: UserVO) => {
+        this.userModel.user = result;
+        this.router.navigate(['/tasks']);
+      },
+      error: (error: HttpErrorResponse) => {
+        this.loginError = error.error.message;
+      }
+    });
+    console.log('onLogin');
+  }
 }

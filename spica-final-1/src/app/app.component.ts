d655cbea88d23b19acef54d769d676990ba4d1eb
Jernej Korosec <jernejk.alfa@gmail.com>
2022-02-08 17:52:24 +0100

    madona, spet bo nočna. Ma spal sm slabih 5 ur.

--- spica-final-1/src/app/app.component.ts
+++ spica-final-1/src/app/app.component.ts
@@ -15,13 +15,82 @@ export class AppComponent implements OnInit {
   constructor(private employeeService: EmployeeService) { }
 
   ngOnInit(): void {
-    this.onGetSession();
-    
-
-    this.onGetEmployee();
+    //this.onGetSession();
+    //this.onGetEmployee();
+    this.testLocalStorage();
   }
 
   
+
+  testLocalStorage(){
+    
+    
+    console.log("testLocalStorage");
+
+    const currentdate1 = new Date('2022-02-08T17:27:59.8914361+01:00');
+    const currentdate2 = new Date();
+    
+    var Difference_In_Time = currentdate2.getTime() - currentdate1.getTime();
+    var Difference_In_Days = Difference_In_Time / (60000);
+    
+    console.log("Session Timestamp:", Difference_In_Days);
+
+
+    if (localStorage.getItem("SpicaApi_Session_Timestamp") === null)
+    {
+      const currentdate = new Date();
+      localStorage.setItem('SpicaApi_Session_Timestamp',currentdate.toString());  
+      var storedTime = localStorage.getItem('SpicaApi_Session_Timestamp')+"";
+      console.log("Time not stored, added to local storage:",storedTime);
+      
+    }
+    else if (localStorage.getItem("SpicaApi_Session_Timestamp") !== null)
+    {
+
+      var storedTime = localStorage.getItem('SpicaApi_Session_Timestamp')+"";
+      
+      console.log("Time stored, fetchedfrom local storage:",storedTime);
+      //localStorage.removeItem('SpicaApi_Session_Timestamp');
+
+    }
+
+
+
+
+
+    if (localStorage.getItem("SpicaApiSessionStartTime") === null) {
+      //console.log("IF");
+      
+      const currentdate = new Date();
+      var datetime = "Last Sync: " + currentdate.getDate() + "/"
+                + (currentdate.getMonth()+1)  + "/" 
+                + currentdate.getFullYear() + " @ "  
+                + currentdate.getHours() + ":"  
+                + currentdate.getMinutes() + ":" 
+                + currentdate.getSeconds();
+      //localStorage.setItem('SpicaApiSessionStartTime',currentdate.toString());  
+      //console.log(`start${currentdate}`);
+
+    }
+    else{
+      //console.log("ELSE");
+      //const currentDateTime = new Date();
+      var storedDateTime = new Date(parseInt(localStorage.getItem('SpicaApiSessionStartTime')+"", 10))
+      //console.log(new Date());
+      // subtract and get time in minutes
+      const currentDateTime = new Date(2020, 2, 1);
+      //const d1 = new Date(2020, 1, 1);
+      const diffMs = +currentDateTime - +storedDateTime;
+      const diffMins = Math.floor((diffMs / 1000) / 60);
+      //console.log(diffMins)
+
+
+    }
+
+    
+    
+  }
+
 
 
 

d04f2fa2be4874237b7112b6f0105516ecb03f93
Jernej Korosec <jernejk.alfa@gmail.com>
2022-02-08 19:37:00 +0100

    Fixed  is 19 minutes passed

--- spica-final-1/src/app/app.component.ts
+++ spica-final-1/src/app/app.component.ts
@@ -17,94 +17,56 @@ export class AppComponent implements OnInit {
   ngOnInit(): void {
     //this.onGetSession();
     //this.onGetEmployee();
-    this.testLocalStorage();
+    //this.testLocalStorage();
+    this.isSessionValid();
   }
 
-  
-
-  testLocalStorage(){
-    
-    
-    console.log("testLocalStorage");
-
-    const currentdate1 = new Date('2022-02-08T17:27:59.8914361+01:00');
-    const currentdate2 = new Date();
-    
-    var Difference_In_Time = currentdate2.getTime() - currentdate1.getTime();
+/// ==============================================================================
+/// ====    RECREATE SESSION IF 19 minutes passed   ==============================
+/// ==============================================================================
+  has19MinsPassed(startDate:Date, endDate:Date ) : boolean
+  {
+    var Difference_In_Time = endDate.getTime() - startDate.getTime();
     var Difference_In_Days = Difference_In_Time / (60000);
-    
-    console.log("Session Timestamp:", Difference_In_Days);
+    if(Difference_In_Days>19){
+      return true;
+    }
+    return false;
+  }
 
-
+  isSessionValid(){
     if (localStorage.getItem("SpicaApi_Session_Timestamp") === null)
     {
-      const currentdate = new Date();
-      localStorage.setItem('SpicaApi_Session_Timestamp',currentdate.toString());  
-      var storedTime = localStorage.getItem('SpicaApi_Session_Timestamp')+"";
-      console.log("Time not stored, added to local storage:",storedTime);
-      
-    }
-    else if (localStorage.getItem("SpicaApi_Session_Timestamp") !== null)
-    {
-
-      var storedTime = localStorage.getItem('SpicaApi_Session_Timestamp')+"";
-      
-      console.log("Time stored, fetchedfrom local storage:",storedTime);
-      //localStorage.removeItem('SpicaApi_Session_Timestamp');
-
-    }
-
-
-
-
-
-    if (localStorage.getItem("SpicaApiSessionStartTime") === null) {
-      //console.log("IF");
-      
-      const currentdate = new Date();
-      var datetime = "Last Sync: " + currentdate.getDate() + "/"
-                + (currentdate.getMonth()+1)  + "/" 
-                + currentdate.getFullYear() + " @ "  
-                + currentdate.getHours() + ":"  
-                + currentdate.getMinutes() + ":" 
-                + currentdate.getSeconds();
-      //localStorage.setItem('SpicaApiSessionStartTime',currentdate.toString());  
-      //console.log(`start${currentdate}`);
-
+      console.log("if");
+      this.onGetSession();
     }
     else{
-      //console.log("ELSE");
-      //const currentDateTime = new Date();
-      var storedDateTime = new Date(parseInt(localStorage.getItem('SpicaApiSessionStartTime')+"", 10))
-      //console.log(new Date());
-      // subtract and get time in minutes
-      const currentDateTime = new Date(2020, 2, 1);
-      //const d1 = new Date(2020, 1, 1);
-      const diffMs = +currentDateTime - +storedDateTime;
-      const diffMins = Math.floor((diffMs / 1000) / 60);
-      //console.log(diffMins)
-
-
+      const storedTimeStr = (localStorage.getItem('SpicaApi_Session_Timestamp')+"").toString();
+      const storedTimeStr2 = Date.parse(storedTimeStr);
+      var storedTime = new Date(storedTimeStr2);
+      var new1 = new Date();
+      var sessionTimeoutOccured = this.has19MinsPassed(storedTime,new1);
+      console.log("\n\r StoredTime:",storedTime,"\n\r NewTime:   ",new1,"\n\r 19minpassed: ",sessionTimeoutOccured);
+      if(sessionTimeoutOccured){
+        this.onGetSession();
+      }
     }
-
-    
-    
   }
 
-
-
+  timeDiffInMinutes(currentdate1: Date,currentdate2: Date):number{
+    var Difference_In_Time = currentdate2.getTime() - currentdate1.getTime();
+    var Difference_In_Days = Math.floor(Difference_In_Time / (60000));
+    return Difference_In_Days;
+  }
 
   onGetSession(): void {
+    console.log("onGetSession(): void");
     this.employeeService.requestSessionToken().subscribe(
       {
         next: (response) => {
-          Globalconstants.Auth_InSession_Token = response.Token;
-          console.log("onGetSession(): Token je:" + Globalconstants.Auth_InSession_Token);
-          
-          //FIXME: Local storage
           const jsonData = JSON.stringify(response.Token)
-          localStorage.setItem('Globalconstants.Auth_InSession_Token',jsonData);
-
+          localStorage.setItem('SpicaApi_Session_Token',jsonData);
+          localStorage.setItem('SpicaApi_Session_Timestamp',new Date().toString());
         },
         error: (e) => console.error('e'),
         complete: () => console.info('complete'),

bc1ea3326d5dac737334c94616bab891dc64c2cb
Jernej Korosec <jernejk.alfa@gmail.com>
2022-02-08 23:47:45 +0100

    Checkout this...

--- my-jqwidgets-ng-app/src/app/app.module.ts
+++ my-jqwidgets-ng-app/src/app/app.module.ts
@@ -1,14 +1,23 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { AppComponent }     from './app.component';
+import { BrowserModule } from '@angular/platform-browser';  
+import { jqxBarGaugeModule }  from 'jqwidgets-ng/jqxbargauge';    //// Added Custom jqwidget component
 
-import { AppComponent } from './app.component';
+import { CommonModule } from '@angular/common';
+import { jqxGridModule } from 'jqwidgets-ng/jqxgrid';
+import { jqxButtonModule } from 'jqwidgets-ng/jqxbuttons';
+import { jqxCheckBoxModule } from 'jqwidgets-ng/jqxcheckbox';
+import { jqxPanelModule } from 'jqwidgets-ng/jqxpanel';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    jqxBarGaugeModule, // Added Custom jqwidget component
+    CommonModule, jqxGridModule, jqxCheckBoxModule, jqxButtonModule, jqxPanelModule // GRID
   ],
   providers: [],
   bootstrap: [AppComponent]

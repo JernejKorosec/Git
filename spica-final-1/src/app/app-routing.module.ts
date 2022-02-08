ba55b02441a4c88e5990ecf42418104e6417a9d3
Jernej Korosec <jernejk.alfa@gmail.com>
2022-02-08 21:06:19 +0100

    Izi pizi.... viewsi narejeni, treba se backend mal popilt pa dokončat....

--- spica-final-1/src/app/app-routing.module.ts
+++ spica-final-1/src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SettingsComponent } from './views/settings/settings.component';
+import { UsersComponent } from './views/users/users.component';
+import { PresenceComponent } from './views/presence/presence.component';
 
-const routes: Routes = [];
-
+//const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/', pathMatch: 'full' },
+  { path: 'settings', component: SettingsComponent },
+  { path: 'users', component: UsersComponent },
+  //{ path: 'users/:id', component: HeroDetailComponent },
+  { path: 'presence', component: PresenceComponent }
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
--- spica-final-1/src/app/app.module.ts
+++ spica-final-1/src/app/app.module.ts
@@ -4,10 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import{ HttpClientModule} from '@angular/common/http';
+import { SettingsComponent } from './views/settings/settings.component';
+import { UsersComponent } from './views/users/users.component';
+import { PresenceComponent } from './views/presence/presence.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SettingsComponent,
+    UsersComponent,
+    PresenceComponent
   ],
   imports: [
     BrowserModule,

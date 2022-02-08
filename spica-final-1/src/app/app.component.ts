7fb70401c051e2ca93d5d0ba5ce7a5a7c30cdebc
Jernej Korosec <jernejk.alfa@gmail.com>
2022-02-08 20:17:22 +0100

    buhahaha.... spanc... niet... nazaj v frontend dev...

--- spica-final-1/src/app/app.component.ts
+++ spica-final-1/src/app/app.component.ts
@@ -12,53 +12,16 @@ export class AppComponent implements OnInit {
   title = 'Spica TimeAPI';
 
   constructor(private employeeService: EmployeeService) { }
-
+  
   ngOnInit(): void {
     //this.onGetSession();
     //this.onGetEmployee();
     //this.testLocalStorage();
-    this.ReLogin();
+    //this.ReLogin();
+    Globalconstants.ReLogin(() => this.onGetSession());
   }
-
-/// ==============================================================================
-/// ====    RECREATE SESSION IF 19 minutes passed   ==============================
-/// ==============================================================================
-  has19MinsPassed(startDate:Date, endDate:Date ) : boolean
-  {
-    var Difference_In_Time = endDate.getTime() - startDate.getTime();
-    var Difference_In_Days = Difference_In_Time / (60000);
-    if(Difference_In_Days>19){
-      return true;
-    }
-    return false;
-  }
-
-  ReLogin(){
-    if (localStorage.getItem("SpicaApi_Session_Timestamp") === null)
-    {
-      console.log("if");
-      this.onGetSession();
-    }
-    else{
-      const storedTimeStr = (localStorage.getItem('SpicaApi_Session_Timestamp')+"").toString();
-      const storedTimeStr2 = Date.parse(storedTimeStr);
-      var storedTime = new Date(storedTimeStr2);
-      var new1 = new Date();
-      var sessionTimeoutOccured = this.has19MinsPassed(storedTime,new1);
-      console.log("\n\r StoredTime:",storedTime,"\n\r NewTime:   ",new1,"\n\r 19minpassed: ",sessionTimeoutOccured);
-      if(sessionTimeoutOccured){
-        this.onGetSession();
-      }
-    }
-  }
-
-  timeDiffInMinutes(currentdate1: Date,currentdate2: Date):number{
-    var Difference_In_Time = currentdate2.getTime() - currentdate1.getTime();
-    var Difference_In_Days = Math.floor(Difference_In_Time / (60000));
-    return Difference_In_Days;
-  }
-
-  onGetSession(): void {
+  
+  onGetSession() {
     console.log("onGetSession(): void");
     this.employeeService.requestSessionToken().subscribe(
       {
@@ -78,7 +41,7 @@ export class AppComponent implements OnInit {
       {
         next: (employeeList) => {
           
-          console.log(employeeList.toString());
+          //console.log(employeeList.toString());
           //console.log("Token je:" + response.Token);
         },
         error: (e) => console.error('e'),
--- spica-final-1/src/app/common/global/globalconstants.ts
+++ spica-final-1/src/app/common/global/globalconstants.ts
@@ -1,19 +1,51 @@
+import { verifyHostBindings } from "@angular/compiler";
 export class Globalconstants {
-    // public static apiURL: string = "https://itsolutionstuff.com/";
-      
-    // public static siteTitle: string = "This is example of ItSolutionStuff.com";
 
     public static spiceApiUrl: string = "http://rdweb.spica.com:5213/timeapi";
-    public static spiceApiEndpoint_session : string = "/Session/GetSession";
-    public static Auth_GetSession_Token : string = "SpicaToken 87F262C4-7FA6-46D3-880D-2F7E15B4F204";
-    public static Auth_InSession_Token? : string = "";
-    public static spiceApiEndpoint_employee : string = "/Employee";
-    public static spiceApiEndpoint_presence : string = "/presence";
+    public static spiceApiEndpoint_session: string = "/Session/GetSession";
+    public static Auth_GetSession_Token: string = "SpicaToken 87F262C4-7FA6-46D3-880D-2F7E15B4F204";
+    public static Auth_InSession_Token?: string = "";
+    public static spiceApiEndpoint_employee: string = "/Employee";
+    public static spiceApiEndpoint_presence: string = "/presence";
 
+    /// ==============================================================================
+    /// ====    RECREATE SESSION IF 19 minutes passed   ==============================
+    /// ==============================================================================
+    static has19MinsPassed(startDate: Date, endDate: Date): boolean {
+        var Difference_In_Time = endDate.getTime() - startDate.getTime();
+        var Difference_In_Days = Difference_In_Time / (60000);
+        if (Difference_In_Days > 19) {
+            return true;
+        }
+        return false;
+    }
 
+    static ReLogin(reEngageSessionCheck: () => void): void {
+        if (localStorage.getItem("SpicaApi_Session_Timestamp") === null) {
+            console.log("if");
+            reEngageSessionCheck();
+        }
+        else {
+            const storedTimeStr = (localStorage.getItem('SpicaApi_Session_Timestamp') + "").toString();
+            const storedTimeStr2 = Date.parse(storedTimeStr);
+            var storedTime = new Date(storedTimeStr2);
+            var new1 = new Date();
+            var sessionTimeoutOccured = Globalconstants.has19MinsPassed(storedTime, new1);
+            console.log("\n\r StoredTime:", storedTime, "\n\r NewTime:   ", new1, "\n\r 19minpassed: ", sessionTimeoutOccured);
+            console.log("\n\r Token:", localStorage.getItem("SpicaApi_Session_Token"));
+            if (sessionTimeoutOccured) {
+                reEngageSessionCheck();
+                console.log("reEngageSessionCheck() called", true);
+            }
+            else{
+                console.log("reEngageSessionCheck() called", false);
+            }
+        }
+    }
 
-
-
-
-    
+    timeDiffInMinutes(currentdate1: Date, currentdate2: Date): number {
+        var Difference_In_Time = currentdate2.getTime() - currentdate1.getTime();
+        var Difference_In_Days = Math.floor(Difference_In_Time / (60000));
+        return Difference_In_Days;
+    }
 }

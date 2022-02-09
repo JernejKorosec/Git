09fbebad1683acd1cc7aa2cd593a4d3022a4089b
Jernej Korosec <jernejk.alfa@gmail.com>
2022-02-09 22:43:15 +0100

    No tole dela... Seveda je pomembno pravilno mapiranje podatkovne strukture...

--- spica-final-1/src/app/app.component.ts
+++ spica-final-1/src/app/app.component.ts
@@ -23,8 +23,8 @@ export class AppComponent implements OnInit {
         [
           { name: 'Id', type: 'number', map: '0' },
           { name: 'LastName', type: 'string', map: '1' },
-         /* { name: 'FirstName', type: 'string', map: '2' },
-          { name: 'MiddleName', type: 'string', map: '3' },
+          { name: 'FirstName', type: 'string', map: '2' },
+          /*{ name: 'MiddleName', type: 'string', map: '3' },
           { name: 'City', type: 'string', map: '4' },
           { name: 'Phone', type: 'string', map: '5' },
           { name: 'Address', type: 'string', map: '6' },
@@ -37,11 +37,17 @@ export class AppComponent implements OnInit {
           { name: 'Unit3', type: 'string', map: '13' },
           { name: 'Unit2', type: 'string', map: '14' },
           { name: 'Unit1', type: 'string', map: '15' },
-          { name: 'Email', type: 'string', map: '16' },
+          */
+          //{ name: 'Email', type: 'string', map: '16' },
+          { name: 'Email', type: 'string', map: '3' },
+          /*
           { name: 'Other', type: 'string', map: '17' },
           { name: 'MobilePhone', type: 'string', map: '18' },
           { name: 'OrganizationalUnitId', type: 'number', map: '19' },
-          { name: 'AdditionalField1', type: 'string', map: '20' },
+          */
+          //{ name: 'AdditionalField1', type: 'string', map: '20' },
+          { name: 'AdditionalField1', type: 'string', map: '4' },
+          /*
           { name: 'AdditionalField2', type: 'string', map: '20' },
           { name: 'AdditionalField3', type: 'string', map: '20' },
           { name: 'AdditionalField4', type: 'string', map: '20' },
@@ -51,7 +57,10 @@ export class AppComponent implements OnInit {
           { name: 'AdditionalField8', type: 'string', map: '20' },
           { name: 'AdditionalField9', type: 'string', map: '20' },
           { name: 'AdditionalField10', type: 'string', map: '20' },
-          { name: 'Active', type: 'boolean', map: '21' },
+          */
+          //{ name: 'Active', type: 'boolean', map: '21' },
+          { name: 'Active', type: 'boolean', map: '5' },
+          /*
           { name: 'InternalField1', type: 'object', map: '22' },
           { name: 'InternalField2', type: 'object', map: '23' },
           { name: 'InternalField3', type: 'object', map: '24' },
@@ -69,13 +78,12 @@ export class AppComponent implements OnInit {
   
     [
       { text: 'ID', datafield: 'Id', width: 100 },
-      { text: 'Priimek', datafield: 'LastName', width: 100 },
-      /*
-      { text: 'Ime', datafield: 'FirstName', width: 150 },
-      { text: 'Email', datafield: 'Email', width: 100 },
+      { text: 'Ime', datafield: 'FirstName', width: 150 },      
+      { text: 'Priimek', datafield: 'LastName', width: 150 },
+      { text: 'Email', datafield: 'Email', width: 200 },
       { text: 'Matična', datafield: 'AdditionalField1', width: 100 },
       { text: 'Active', datafield: 'Active' }
-      */
+      
     ];
 
 
@@ -145,27 +153,14 @@ export class AppComponent implements OnInit {
     this.employeeService.getEmployees().subscribe(
       {
         next: (employeeList) => {
-          //console.table(employeeList[0]);
           result = employeeList;
-          //this.source.localdata = result;
-          //let a = this.somefunction(employeeList);
-          //console.log(`Type of source1 ${employeeList}:`, typeof employeeList);
-          
           const sel: Object[] = employeeList.map(employee =>  ({Id: employee.Id, Firstname: employee.FirstName })); // no error
-          const newList = employeeList.map(employee => [employee.Id, employee.FirstName]);
-
+          const newList = employeeList.map(employee => [employee.Id, employee.FirstName, employee.LastName, employee.Email, employee.AdditionalField1, employee.Active]);
           console.log(`sel is typeof: ${typeof sel}, and value is:`,sel);
           console.log(`newList is typeof: ${typeof newList}, and value is:`,newList);
           const usersJson: any[] = Array.of(sel);
           console.log("Jiiiiiihaaaaaaa:",usersJson);
-
-          //var employeeEntries: [string, number][] = [];
-          //var shortAtributeemployeeList2: [string, number][] = employeeList.map( employee =>  ({Id: employee.Id, Firstname: employee.FirstName })); // no error
-          
-
-
           //debugger
-          //this.source.localdata = a;
           let source1 =
           {
               localdata: [
@@ -179,49 +174,30 @@ export class AppComponent implements OnInit {
           let a2 = source1.localdata;
           let a3 = source1.localdata[0];
           let a4 = source1.localdata[0][0];
-
           console.log(`Type of source1 ${a1}:`, typeof a1);
           console.log(`Type of source1.localdata ${a2}:`, typeof a2);
           console.log(`Type of source1.localdata[0] ${a3}:`, typeof a3);
           console.log(`Type of source1.localdata[0][0] ${a4}:`, typeof a4);
-          
           console.log("TYPEOF---> :", typeof source1.localdata[0][0]);
           */
-
           //this.source.localdata = result;
           let bbb = source1.localdata;
           console.log(`bbb is typeof: ${typeof bbb}, and value is:`,bbb);
           //this.source.localdata = bbb;
           this.source.localdata = newList;
-
-
           //this.source.localdata = shortAtributeemployeeList;
-          
-
-
-
           //let watch1 = result;
           //let watch2 = this.somefunction(employeeList);
           //debugger;
-
           //console.log(Object.getOwnPropertyNames(result[0]));
           //let defs = Object.getOwnPropertyNames(result[0]);
-          
-          //this.columns
+           //this.columns
           //console.log(this.columns.keys.name);
-
-
-          //console.log("---   {",result[0].Email,"}   ---");
-
+         //console.log("---   {",result[0].Email,"}   ---");
           //Object.getOwnPropertyNames(result[0])
-
           //let neki = result[0].Email;
           //console.log("---   {",neki,"}   ---");    // email
-
-          
           //  this.source.localdata = neki;
-
-
           //this.dataAdapter = employeeList;
           //console.log("----------------------------");
           /*
@@ -230,15 +206,12 @@ export class AppComponent implements OnInit {
           let object1 = JSON.stringify(testsource1, null, 4);
           let object1String = testsource1[0];
           let object2String = JSON.stringify(object1String, null, 4);
-
-
           //console.count(Object.keys(this.source.localdata));
           console.log(object1String);
           console.log(object2String);
           */
-         //console.log(this.source.datafield);
+          //console.log(this.source.datafield);
           //console.log("----------------------------");
-
           //this.source.localdata = object1;
           this.dataAdapter = new jqx.dataAdapter(this.source);
           //this.dataAdapter = new jqx.dataAdapter(object2String);
